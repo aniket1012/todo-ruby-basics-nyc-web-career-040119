ba8d313c5d1418c452eaf030cf56b9129de8e81c
Aniket Patel <aniketupatel1@gmail.com>
2019-03-27 00:38:44 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -18,8 +18,8 @@ def return_a_value(phrase = "Nice")
 return phrase
 end
 
-def last_evaluated_value(phrase = "expert")
-  return phrase
+def last_evaluated_value(phrase)
+
 end
 
 def pizza_party(topping = "cheese")
